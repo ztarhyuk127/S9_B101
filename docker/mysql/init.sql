30e360e8ef2e31cf4a8cc15a7e5fe0568860efe5
강동혁 <ztarhyuk127@gmail.com>
2023-10-16 13:42:53 +0900

    반출하기 위해 소스 삭제

--- docker/mysql/init.sql
+++
@@ -1,9 +0,0 @@
-drop user  'root'@'%';
-
-create user 'b101'@'%' identified by 'fjdjf384ohtfea838oa';
-create user 'b101'@'localhost' identified by 'fjdjf384ohtfea838oa';
-
-grant all privileges on *.* to 'b101'@'%';
-grant all privileges on *.* to 'b101'@'localhost';
-
-use onedb;
